9f46a57e4441afc740c3ac614b859d8b27ba09c5
DuskWarden <pawar96sameer@gmail.com>
2025-10-16 00:43:31 +0530

    changes in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
-          📝 NotesApp
+           NotesApp
         </Link>
         
         <div className="navbar-menu">
